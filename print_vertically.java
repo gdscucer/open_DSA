89768605465acba7d3773408f7ed6cdd28e7c559
SonakshiChauhan <91408631+SonakshiChauhan@users.noreply.github.com>
2022-10-03 11:16:16 +0530

    Create print_vertically.java

---
+++ print_vertically.java
@@ -0,0 +1,25 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Print_words_vertically {
+    class Solution {
+        public List<String> printVertically(String s) {
+            String[] arr=s.split(" ");
+            int max=0;
+            for(String v:arr){
+                max=Math.max(max,v.length());
+            }
+
+            List<String> ret=new ArrayList();
+            for(int i=0;i<max;i++){
+                String v="";
+                for(int j=0;j<arr.length;j++){
+                    int len=arr[j].length();
+                    v+=(len<max && i>=len)?" ": arr[j].charAt(i);
+                }
+                ret.add(v.replaceAll("\\s+$",""));
+            }
+            return ret;
+        }
+    }
+}
