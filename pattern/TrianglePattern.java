6abcd65da3a2a84a3c68b872c20163af85804eb9
AyushAhuja2707 <69260450+AyushAhuja2707@users.noreply.github.com>
2022-10-14 00:26:09 +0530

    Create TrianglePattern.java

---
+++ pattern/TrianglePattern.java
@@ -0,0 +1,28 @@
+/* 
+        *
+      *   *
+    *       *
+  *           *
+*               *
+*/
+public class Triangle13 {
+  public static void main(String[] args) {
+    // Write code here
+    int rows = 5;
+    for (int i = 1; i <= rows; i++) {
+      for (int j = rows; j > i; j--) {
+        System.out.print(" ");
+      }
+      System.out.print("*");
+      for (int j = 1; j < (i - 1) * 2; j++) {
+        // prints space
+        System.out.print(" ");
+      }
+      if (i == 1) {
+        System.out.print("\n");
+      } else {
+        System.out.print("*" + "\n");
+      }
+    }
+  }
+}
